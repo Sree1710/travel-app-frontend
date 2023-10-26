6ca3a0da7857dda7eb659f5ffd6eaa9aa13d041e
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-26 20:38:33 +0530

    ViewTourPack component completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import AddTourPack from './Components/AddTourPack';
+import ViewTourPack from './Components/ViewTourPack';
 
 function App() {
   return (
     <div>
-      <AddTourPack/>
+      <ViewTourPack/>
     </div>
   );
 }
