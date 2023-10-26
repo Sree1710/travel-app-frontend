9f06325ebdf1501da20dedbfdf8ad15a8b36fc2f
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-26 22:17:52 +0530

    NavBar component completed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Travel App</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/addt">Add Tour Pack</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/viewt">View Tour Pack</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/searcht">Search Tour Pack</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default NavBar
