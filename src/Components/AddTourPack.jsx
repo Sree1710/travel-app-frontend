e83d8fab3d0e0615ba3e82a382cdfda491a7204b
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-26 20:39:59 +0530

    modification done

--- src/Components/AddTourPack.jsx
+++ src/Components/AddTourPack.jsx
@@ -35,6 +35,7 @@ const AddTourPack = () => {
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <h1>Add Tour Package</h1>
                         </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"></div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Package ID</label>
                             <input onChange={inputHandler} type="text" className="form-control" name="packID" value={inputField.packID} />
