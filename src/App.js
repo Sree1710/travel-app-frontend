cb91732e3fd6aee8e481279b96d3c5b92d36724f
Sree1710 <sreelekshmisl1710@gmail.com>
2023-10-26 22:12:59 +0530

    Login component completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import AddTourPack from './Components/AddTourPack';
 import ViewTourPack from './Components/ViewTourPack';
 import SearchTourPack from './Components/SearchTourPack';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div>
-      <SearchTourPack/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={<Login/>}/>
+        <Route path='/addt' exact element={<AddTourPack/>}/>
+        <Route path='/viewt' exact element={<ViewTourPack/>}/>
+        <Route path='/searcht' exact element={<SearchTourPack/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
